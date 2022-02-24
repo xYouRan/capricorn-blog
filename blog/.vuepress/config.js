56e7d3cd8a6c557f478ccdd6c3b146d03152a61f
ranran <wf1415682576@2980.com>
2022-02-24 10:33:35 +0800

    项目发布

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,9 +1,53 @@
 module.exports = {
   title: '摩羯 Blog', // Title for the site. This will be displayed in the navbar.
   theme: '@vuepress/theme-blog',
+  base: "/capricorn-blog/",
   themeConfig: {
-    // Please keep looking down to see the available options.
-  }
+
+    dateFormat: 'YYYY-MM-DD',
+
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#footer
+     */
+    footer: {
+      contact: [
+        {
+          type: 'github',
+          link: 'https://github.com/jenkey2011',
+        }
+      ],
+      copyright: [{
+        text: 'Privacy Policy',
+        link: 'https://policies.google.com/privacy?hl=en-US',
+      },
+      {
+        text: 'MIT Licensed | Copyright © 2018-present Vue.js',
+        link: '',
+      },
+      ],
+    },
+    directories: [
+      {
+        id: 'post',
+        dirname: '_posts',
+        path: '/',
+        itemPermalink: '/:year/:month/:day/:slug',
+      },
+      {
+        id: 'writing',
+        dirname: '_writings',
+        path: '/',
+        itemPermalink: '/:year/:month/:day/:slug',
+      },
+    ],
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#sitemap
+     */
+    sitemap: {
+      hostname: 'https://xxx.com/'
+    },
+    smoothScroll: true
+  },
 }
 
 
